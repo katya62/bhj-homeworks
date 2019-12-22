f64ab1346b85e14d370876b3941a36c097d0d8d9
Kachalina, Ekaterina <katyaka2016@gmail.com>
2019-12-22 16:48:52 +0300

    Таймер по окончанию показывал 1- исправлено

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -3,7 +3,9 @@ function timer(){
 
  obj.innerHTML--;
 
- if(obj.innerHTML==0){alert('Вы победили в конкурсе!');setTimeout(function(){},100);}
- else{setTimeout(timer,100);}
+ if(obj.innerHTML == 0) {
+ 	setTimeout (() => alert('Вы победили в конкурсе!'),0);
+ }
+ else {setTimeout(timer,100);}
 }
 setTimeout(timer,100);
